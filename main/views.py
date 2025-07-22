d2f6bf7e0df3e39fd46d658ec81eb11a4b80749d
i_am_ZhVi <unius6ls66@gmail.com>
2025-07-22 23:15:09 +0300

    reforming pages

--- main/views.py
+++ main/views.py
@@ -4,15 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Main",
-        "list": ['first', 'second'],
-        "dict": {"first": 1},
-        "is_authenticated": True,
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME"
+
     }
 
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return render(request, "main/about.html")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Text classsni ochen mnogo"
+    }
+    return render(request, "main/about.html", context)
