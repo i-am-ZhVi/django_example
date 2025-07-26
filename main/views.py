7349f4cf26f296efba80f7cf347c8bcfef064af8
i_am_ZhVi <unius6ls66@gmail.com>
2025-07-26 12:23:04 +0300

    create fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 # Create your views here.
 def index(request):
     context = {
         "title": "Home - Главная",
-        "content": "Магазин мебели HOME"
-
+        "content": "Магазин мебели HOME",
+        "categories": Categories.objects.all()
     }
 
     return render(request, "main/index.html", context)
