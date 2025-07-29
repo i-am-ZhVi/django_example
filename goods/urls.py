1b1485bd9a9e4e9fe0f338ab0db80be2d395916b
i_am_ZhVi <unius6ls66@gmail.com>
2025-07-29 16:06:33 +0300

    first pagination

--- goods/urls.py
+++ goods/urls.py
@@ -1,3 +1,4 @@
+from goods.views import catalog
 from django.contrib import admin
 from django.urls import path
 
@@ -7,5 +8,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name="index"),
+    path("<slug:category_slug>/<int:page>/", views.catalog, name="index"),
     path("product/<slug:product_slug>/", views.product, name="product"),
 ]
