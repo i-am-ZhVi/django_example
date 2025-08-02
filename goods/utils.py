c8ef4d40a43b07f89501016579177f028b015172
i_am_ZhVi <unius6ls66@gmail.com>
2025-08-02 11:58:21 +0300

    full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,10 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import (
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+    SearchHeadline
+)
 
 from goods.models import Products
 def q_search(query):
@@ -11,9 +16,31 @@ def q_search(query):
     query = SearchQuery(query)
 
 
-    return Products.objects.annotate(
+    result = Products.objects.annotate(
         rank=SearchRank(vector, query)
-    ).order_by("-rank")
+    ).filter(rank__gt=0).order_by("-rank")
+
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel="<span style='background-color: yellow;' >",
+            stop_sel="</span>"
+        )
+    )
+
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel="<span style='background-color: yellow;' >",
+            stop_sel="</span>"
+        )
+    )
+
+
+    return result
 
     # keywords = [word for word in query.split() if len(word) > 2]
 
